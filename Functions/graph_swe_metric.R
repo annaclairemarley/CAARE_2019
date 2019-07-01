24877dca6e212ef7480d7cba71967c3465a6b761
Marley <armarley@ndc.nasa.gov>
2019-07-01 08:53:06 -0700

    simplified the functions down to 2

---
+++ Functions/graph_swe_metric.R
@@ -0,0 +1,26 @@
+#' graph_swe_metric
+#' 
+#' Graphs your swe metrics
+#'
+#' @param grouped_region the dataframe of the grouped summary statistic created w/ calc_swe_metrics()
+#' @param type type of geom plot. examples are geom_line, geom_point, geom_col etc
+#'
+#' @return visualization of data
+#' @export
+#'
+#' @examples
+
+graph_swe_metric = function(grouped_region, type){
+  graph = grouped_region %>% 
+    ggplot(aes(x = date, y = swe_mm)) + 
+    type() +
+    scale_y_continuous(expand = c(0,0)) +
+    labs(
+      x = "Year",
+      y = "SWE (mm)"
+    ) +
+    theme_classic() 
+  
+  return(swe_graph = graph)
+  
+}
