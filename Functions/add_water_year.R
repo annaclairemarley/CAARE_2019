09a48faa18ba73a82a55bc7de0778e6e93c45713
Ignacy Debicki <itdebicki@gmail.com>
2019-07-02 22:27:15 -0700

    Add changed files

--- Functions/add_water_year.R
+++ Functions/add_water_year.R
@@ -12,23 +12,23 @@
 add_water_year = function(df) {
   
   wy_df = df %>% 
-    dplyr:: mutate(Date = date) %>%
+    dplyr:: rename(Date = date) %>%
     dataRetrieval::addWaterYear() %>% 
     dplyr::mutate(month = month(Date),
            day = day(Date)) 
   
-    for (i  in 1:length(wy_df$month)) {
-      wy_df$month[i] = ifelse(wy_df$month[i] == "4", paste("06"), wy_df$month[i])
-      wy_df$month[i] = ifelse(wy_df$month[i] == "3", paste("05"), wy_df$month[i])
-      wy_df$month[i] = ifelse(wy_df$month[i] == "2", paste("04"), wy_df$month[i])
-      wy_df$month[i] = ifelse(wy_df$month[i] == "1", paste("03"), wy_df$month[i])
-      wy_df$month[i] = ifelse(wy_df$month[i] == "11", paste("01"), wy_df$month[i])
-      wy_df$month[i] = ifelse(wy_df$month[i] == "12", paste("02"), wy_df$month[i])
-    }
+  #for (i  in 1:length(wy_df$month)) {
+  #  wy_df$month[i] = ifelse(wy_df$month[i] == "4", paste("06"), wy_df$month[i])
+  #  wy_df$month[i] = ifelse(wy_df$month[i] == "3", paste("05"), wy_df$month[i])
+  #  wy_df$month[i] = ifelse(wy_df$month[i] == "2", paste("04"), wy_df$month[i])
+  #  wy_df$month[i] = ifelse(wy_df$month[i] == "1", paste("03"), wy_df$month[i])
+  #  wy_df$month[i] = ifelse(wy_df$month[i] == "11", paste("01"), wy_df$month[i])
+  #  wy_df$month[i] = ifelse(wy_df$month[i] == "12", paste("02"), wy_df$month[i])
+  #}
     
-    wy_df = wy_df %>% 
-      mutate(date = ymd(paste(waterYear, month, day, sep="-"))) %>% 
-      dplyr::select(date, SWE_mm) 
+   wy_df = wy_df %>% 
+     mutate(wy_date = ymd(paste(waterYear, month, day, sep="-"))) %>% 
+     dplyr::select(wy_date, Date, SWE_mm) 
   
   return(water_year_df = wy_df)
 }
--- Functions/graph_swe_daily.R
+++ Functions/graph_swe_daily.R
@@ -12,17 +12,23 @@
 
 graph_swe_daily = function(df, region){
   
+  startYear = year(min(df$wy_date))
+  endYear = year(max(df$wy_date))
+  labelDates = seq.Date(from = ymd(paste(year(min(df$wy_date)),11,1, sep='-')), length.out = endYear - startYear + 1, by = "1 year")
+  breakDates = seq.Date(from = ymd(paste(year(min(df$wy_date)) - 1,11,1, sep='-')), length.out = endYear - startYear + 1, by = "1 year")
+  
   swe_daily_graph = df %>% 
-    ggplot(aes(x = date, y = SWE_mm)) +
+    ggplot(aes(x = Date, y = SWE_mm)) +
     geom_line() +
     scale_y_continuous(expand = c(0,0)) +
-    scale_x_date(date_breaks = "2 year") +
+    scale_x_date(breaks = unlist(breakDates, recursive = FALSE), labels=unlist(labelDates, recursive = FALSE)) +
     labs(
       x = "Year",
       y = "SWE (mm)",
       title = sprintf("%s", region)
     ) +
-    theme_classic() 
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
   
   return(swe_daily = swe_daily_graph)
  }
