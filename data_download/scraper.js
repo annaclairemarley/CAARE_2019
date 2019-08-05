599a159ae60dd0dbf89e756f1038fe25ab5a9793
AnnaClaire Marley <annaclairemarley@gmail.com>
2019-08-05 11:11:39 -0700

    updated scraper

--- data_download/scraper.js
+++ data_download/scraper.js
@@ -383,7 +383,7 @@ puppeteer.launch({headless: false}).then(browser => {
     // getData(regions.chapters, true, "2003-09-01", "2019-07-17", "pdsi", "Date,Palmer Drounght Severity Index").then(() => {
     //     browser.close();
     // }).catch(e => console.log(e));
-    getData(["Middle San Juan"], regionType.FUSION_TABLE,"2013-02-25", "2019-07-17", "CHIRPS_daily", ["1SCbzTWYImyozb3zBt3q2XpSVeF0oXVbF9U0pMujm"], "Date,Precipitation").then(() => {
+    getData(["Middle San Juan"], regionType.FUSION_TABLE,"2013-12-13", "2019-07-17", "CHIRPS_daily", ["1SCbzTWYImyozb3zBt3q2XpSVeF0oXVbF9U0pMujm"], "Date,Precipitation").then(() => {
         browser.close();
     }).catch(e => console.log(e));
     
